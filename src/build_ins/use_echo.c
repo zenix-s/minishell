b8b2400a766ed2eadd7f697158595a0ffbc4e0ca
Lucas Ortega Morata <lucasmorataortega@gmail.com>
2025-02-08 19:16:12 +0100

    comando echo finish
    creo que funciona entero

--- src/build_ins/use_echo.c
+++ src/build_ins/use_echo.c
@@ -1,63 +1,39 @@
 
 #include "../../include/minishell.h"
 
-/*
-static void	putvar(char *content)
-{
-	int		count;
-	int		othercount;
-	char	*linewrite;
-	int		new_size;
-
-	count = 0;
-	othercount = 0;
-	while (content[count] != '=')
-		count++;
-	count++;
-	new_size = ft_strlen(content) - count;
-	linewrite = (char *)ft_calloc(new_size + 1, sizeof(char));
-	while (content[count])
-	{
-		linewrite[othercount] = content[count];
-		count++;
-		othercount++;
-	}
-	printf("%s ", linewrite);
-	free(linewrite);
-}
-
-static void	onedolar(t_token *l_aux, char *line)
+static void	print_echo(char **line_arraid, int arg)
 {
 	char	*aux;
-	char	*content;
 
-	while (l_aux)
+	while (line_arraid[arg])
 	{
-		aux = ft_strtrim(line, "$");
-		content = (char *) l_aux->content;
-		if (ft_strncmp(content, aux, size_env(content)) == 0)
-			putvar(content);
+		aux = (ft_strtrim(line_arraid[arg], "\""));
+		if (aux != NULL)
+			printf("%s ", aux);
 		free(aux);
-		l_aux = l_aux->next;
+		arg++;
 	}
 }
-*/
+
 void	use_echo(char **line_arraid)
 {
 	int		arg;
-	char	*aux;
+	int		bool;
+	char	*cut;
 
-	if (ft_strncmp(line_arraid[1], "-n", 1) == 0)
+	cut = ft_strtrim (line_arraid[1], "n");
+	bool = 0;
+	if (!line_arraid[1])
+		return ;
+	if (ft_strcmp(cut, "-") == 0)
+	{
 		arg = 2;
+		bool = 1;
+	}
 	else
 		arg = 1;
-		//l_aux = list_env;
-		//if (line_arraid[arg][0] == '$' && line_arraid[arg][1] != '$')
-		//	onedolar(l_aux, line_arraid[arg]);
-		//else
-	aux = (ft_strtrim(line_arraid[arg], "\""));
-	printf("%s ", aux);
-	free(aux);
-	if (ft_strncmp(line_arraid[1], "-n", 1) != 0)
+	free(cut);
+	print_echo(line_arraid, arg);
+	if (bool == 0)
 		printf("\n");
 }
