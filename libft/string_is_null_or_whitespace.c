bd57cf8ce3ecb328f5b1c2e66ef6743342345182
zenix-s <sergioffdev@gmail.com>
2025-03-22 22:55:17 +0100

    Norma

--- libft/string_is_null_or_whitespace.c
+++ libft/string_is_null_or_whitespace.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string_is_null_or_whitespace.c                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: serferna <serferna@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/26 11:13:15 by serferna          #+#    #+#             */
+/*   Updated: 2025/01/26 11:13:15 by serferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/minishell.h"
 
 t_bool string_is_null_or_whitespace(const char *str)
