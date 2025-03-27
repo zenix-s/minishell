b8db5515590d67eeb1ba5b8d59de529012192520
Lucas Ortega Morata <lucasmorataortega@gmail.com>
2025-03-27 01:32:33 +0100

    quitar errores raros

--- src/state/segurity_state.c
+++ src/state/segurity_state.c
@@ -12,12 +12,35 @@
 
 #include "../../include/minishell.h"
 
+static void	created_files(t_shell *shell)
+{
+	t_token	*aux_token;
+	int		file;
+
+	aux_token = shell->tokens;
+	while (aux_token)
+	{
+		if (newcmp(aux_token->content, ">") == 0)
+		{
+			file = open(aux_token->next->content, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+			close(file);
+		}
+		if (newcmp(aux_token->content, ">>") == 0)
+		{
+			file = open(aux_token->next->content, O_CREAT | O_WRONLY | O_APPEND, 0644);
+			close(file);
+		}
+		aux_token = aux_token->next;
+	}
+}
+
 void	segurity_state(t_shell *shell)
 {
 	if (shell->input && *shell->input != '\0')
 	{
 		if (ft_strlen(shell->input) > MAX_INPUT_LENGTH)
 			ft_error("Error: line so long.\n");
+		created_files(shell);
 		all_heredoc(shell);
 		if (g_exit_status == 330)
 		{
